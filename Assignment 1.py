e737963641b02974744cbed496c83379722425da
Caleb Zappala <caleb.zappala@my.jcu.edu.au>
2016-09-09 14:32:32 +1000

    Final Format

--- Assignment 1.py
+++ Assignment 1.py
@@ -4,7 +4,7 @@ Caleb Zappala
 Started 2/9/2016
 Shopping list: Opens a list of items and allows the user to either, look at required items, look at completed items,
 add new item  and marks a item as completed.
-https://github.com/jc320863/CP1404_Assignment1/commit/db0d7ceda9f38323df84d08bf28911b8c6170961
+https://github.com/jc320863/CP1404_Assignment1
 
 Pseudocode:
 
@@ -13,14 +13,14 @@ function load_items()
     open file using csv reader
     for rows in csv file
         add each row to the blank items list
-    sort items_list according to colom 2 in accending order
+    sort items_list according to column 2 in accending order
     return items list
 
 function display_completed_items
     create new variable total_cost_completed and let equal to zero
     create blank list called completed_list
     for items in range from zero to length of initial items list
-        if within initial items, referencing a list within a list, check the fourth coloum and see if the string 'c'
+        if within initial items, referencing a list within a list, check the fourth column and see if the string 'c'
                                                             is present
             add the items to the comp list from the initial items list
             total_cost_completed = float of total_cost_completed + cost of item in items list
@@ -31,19 +31,21 @@ function display_completed_items
             print formatted string with completed, completed_list zero, completed_list one, completed_list two
         print formatted string for total expected price with a format for the length of completed_list and the variable
          total_cost_completed
-
 """
 import csv
 
 
 def main():
+    """
+    Shopping list program,
+    determine shopping list based on required items, completed items and any new items which wish to be added
+    """
     items_list = load_items()
 
     print("Shopping list 1.0 - Caleb Zappala \n{} items loaded from items.csv".format(len(items_list)))
     menu = "Menu: \nR - List required items\nC - List completed items\nA - Add new item\n" \
            "M - Mark an item as completed\nQ (for quit)"
     print(menu)
-
     choice = input(">>> ").upper()
 
     while choice != "Q":
@@ -70,9 +72,11 @@ def main():
 
 
 def marking_item_as_completed(req_list):
+    """ get details for marking item; error checking items specified to be marked ; print item marked """
     print("Enter the number of items to be completed ")
     while True:
         try:
+
             specify_number_of_item_to_be_marked = int(input(">>> "))
 
             if specify_number_of_item_to_be_marked >= 0 and specify_number_of_item_to_be_marked < len(req_list):
@@ -81,11 +85,13 @@ def marking_item_as_completed(req_list):
                 print("Invalid item number ")
         except ValueError:
             print("Invalid input; enter a number")
+    # setting item number specified to value c to now make the items completed
     req_list[specify_number_of_item_to_be_marked][3] = 'c'
     print("{} marked as completed".format(req_list[specify_number_of_item_to_be_marked][0]))
 
 
 def add_new_item(items_list):
+    """ create new items list; specify each column with inputs while error checking ; add new item to items list """
     new_item = [0, '0', 0, 0]
     new_item[3] = 'r'
     new_item[0] = str(input("Item name: ").strip())
@@ -120,12 +126,16 @@ def add_new_item(items_list):
 
 
 def save_items(items_list):
+    """ using the csv writer command and overwrite exsisting items.csv file """
     save_item = csv.writer(open("items.csv", 'w', newline=''))
     for item in items_list:
         save_item.writerow(item)
 
 
 def display_completed_items(items_list):
+    """ create blank list to completed items and a variable for total cost; go through items list looking for
+    completed items saving to completed list and summing cost for each completed item; check a empty list isn't present;
+    neatly format completed items and total cost """
     total_cost_completed = 0
     completed_list = []
     for com_items in range(0, len(items_list)):
@@ -142,6 +152,9 @@ def display_completed_items(items_list):
 
 
 def display_required_items(items_list):
+    """ create blank list to required items and a variable for total cost; go through items list looking for
+    required items saving to required list and summing cost for each required item; check a empty list isn't present;
+    neatly format required items and total cost """
     total_cost_req = 0
     required_list = []
     for item in range(0, len(items_list)):
@@ -159,6 +172,7 @@ def display_required_items(items_list):
 
 
 def load_items():
+    """ using the csv reader command open items.csv file and read into items list """
     items_list = []
     file_open = csv.reader(open("items.csv"))
     for row in file_open:
